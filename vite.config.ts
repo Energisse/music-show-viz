17a17dd370b0b68ac9bde361c6c5830b19a3db20
Energisse <thomas.halvick@gmail.com>
2025-01-08 18:56:54 +0100

    github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+
+  }
 })
