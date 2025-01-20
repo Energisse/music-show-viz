af645604a06595c4228d1d8aac96689064ab8b50
Energisse <thomas.halvick@gmail.com>
2025-01-20 23:19:53 +0100

    Simplification radar

--- src/Radar.tsx
+++ src/Radar.tsx
@@ -30,31 +30,59 @@ export default function Radar({ zoomed }: RadarProps) {
     return selectedUsersRaw;
   }, [selectedUsersRaw, order]);
 
-  const allAxes = useMemo(() => {
-    const result = selectedUsers
-      .map((user) => data.users.find((u) => u.user_id === user))
-      .filter(Boolean)
-      .flatMap((userData) => {
-        // Utiliser la fonction getTopGenres pour récupérer les genres de l'utilisateur pour la période sélectionnée
-        // Trouver la période correspondant à "period"
-        const selectedPeriod =
-          userData!.top_genres.find((p) => p.label === period)?.ranking || [];
+  const [allAxes, userData] = useMemo(() => {
+    const axes = Array.from(
+      new Set(
+        selectedUsers
+          .map((user) => data.users.find((u) => u.user_id === user))
+          .flatMap((userData) => {
+            // Utiliser la fonction getTopGenres pour récupérer les genres de l'utilisateur pour la période sélectionnée
+            // Trouver la période correspondant à "period"
+            const selectedPeriod =
+              userData!.top_genres.find((p) => p.label === period)?.ranking ||
+              [];
 
-        const topGenres = selectedPeriod
-          .flatMap((genre) => ({
-            name: genre.Tags,
-            time: genre["Listening Time"],
-          }))
-          .sort((a, b) => b.time - a.time)
-          .slice(0, topN)
-          .map((genre) => genre.name)
-          .filter(Boolean);
+            return selectedPeriod
+              .flatMap((genre) => ({
+                name: genre.Tags,
+                time: genre["Listening Time"],
+              }))
+              .sort((a, b) => b.time - a.time)
+              .slice(0, topN)
+              .map((genre) => genre.name);
+          })
+      )
+    );
 
-        return topGenres;
-      });
+    const userData = selectedUsers.map((user) => {
+      const userData = data.users.find((u) => u.user_id === user)!;
 
-    // Retourner les genres sous forme de tableau
-    return Array.from(new Set(result));
+      const selectedPeriod = userData.top_genres
+        .find((p) => p.label === period)
+        ?.ranking.filter((g) => axes.includes(g.Tags))
+        .sort((a, b) => axes.indexOf(a.Tags) - axes.indexOf(b.Tags));
+
+      const total = d3.sum(
+        userData.top_genres.find((p) => p.label === period)?.ranking || [],
+        (d) => d["Listening Time"]
+      );
+
+      return {
+        user_id: userData.user_id,
+        username: userData.username,
+        genres: axes.map((name) => {
+          const genre = selectedPeriod?.find((g) => g.Tags === name);
+          return {
+            name,
+            proportion: ((genre?.["Listening Time"] || 0) / total) * 100,
+            time: genre?.["Listening Time"] || 0,
+            formatedTime: formatListenTime(genre?.["Listening Time"] || 0),
+          };
+        }),
+      };
+    });
+
+    return [axes, userData];
   }, [selectedUsers, period, topN]);
 
   useEffect(() => {
@@ -72,7 +100,7 @@ export default function Radar({ zoomed }: RadarProps) {
   useEffect(() => {
     const margin = 50;
 
-    const pointSize = (zoom) => Math.max(1, Math.min(5 - zoom, 4));
+    const pointSize = (zoom: number) => Math.max(1, Math.min(5 - zoom, 4));
 
     //clear the chart
     d3.select(container.current).select(".chart").selectAll("*").remove();
@@ -105,232 +133,202 @@ export default function Radar({ zoomed }: RadarProps) {
     const angleSlice = (2 * Math.PI) / allAxes.length;
 
     const gridLevels = 5;
-    for (let i = 0; i <= gridLevels; i++) {
-      const levelRadius = (radius / gridLevels) * i;
 
-      radarGroup
-        .append("circle")
-        .attr("cx", centerX)
-        .attr("cy", centerY)
-        .attr("r", levelRadius)
-        .style("fill", "none")
-        .style("stroke", "#ccc");
+    radarGroup
+      .selectAll("circle")
+      .data(Array.from({ length: gridLevels + 1 }))
+      .join("circle")
+      .attr("cx", centerX)
+      .attr("cy", centerY)
+      .attr("r", (_, i) => (radius / gridLevels) * i)
+      .style("fill", "none")
+      .style("stroke", "#ccc");
 
-      radarGroup
-        .append("text")
-        .attr("x", centerX)
-        .attr("y", centerY - levelRadius - 5)
-        .attr("text-anchor", "middle")
-        .attr("font-size", "10px")
-        .attr("fill", "#666")
-        .text((100 / gridLevels) * i);
-    }
+    radarGroup
+      .selectAll("text")
+      .data(Array.from({ length: gridLevels + 1 }))
+      .join("text")
+      .attr("x", centerX)
+      .attr("y", (_, i) => centerY - (radius / gridLevels) * i - 5)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "10px")
+      .attr("fill", "#666")
+      .text((_, i) => (100 / gridLevels) * i);
 
-    // Ajouter les axes (labels) dynamiquement en fonction des genres
-    allAxes.forEach((genre: string, i) => {
-      const angle = angleSlice * i + (3 * Math.PI) / 2;
-      const x = centerX + Math.cos(angle) * radius;
-      const y = centerY + Math.sin(angle) * radius;
+    // // Ajouter les axes (labels) dynamiquement en fonction des genres
 
-      radarGroup
-        .append("line")
-        .attr("x1", centerX)
-        .attr("y1", centerY)
-        .attr("x2", x)
-        .attr("y2", y)
-        .attr("stroke", "#ccc");
+    radarGroup
+      .selectAll("line")
+      .data(allAxes)
+      .join("line")
+      .attr("x1", centerX)
+      .attr("y1", centerY)
+      .attr(
+        "x2",
+        (d, i) =>
+          centerX + Math.cos(angleSlice * i + (3 * Math.PI) / 2) * radius
+      )
+      .attr(
+        "y2",
+        (d, i) =>
+          centerY + Math.sin(angleSlice * i + (3 * Math.PI) / 2) * radius
+      )
+      .attr("stroke", "#ccc");
 
-      const labelX = centerX + Math.cos(angle) * (radius + 20);
-      const labelY = centerY + Math.sin(angle) * (radius + 20);
+    axisLabelsGroup
+      .selectAll("text")
+      .data(allAxes)
+      .join("text")
+      .attr(
+        "x",
+        (_, i) =>
+          centerX + Math.cos(angleSlice * i + (3 * Math.PI) / 2) * (radius + 20)
+      )
+      .attr(
+        "y",
+        (_, i) =>
+          centerY + Math.sin(angleSlice * i + (3 * Math.PI) / 2) * (radius + 20)
+      )
+      .attr("text-anchor", "middle")
+      .attr("font-size", "12px")
+      .attr("fill", "#666")
+      .attr("class", "axis-label")
+      .text((genre) => genre);
 
-      axisLabelsGroup
-        .append("text")
-        .attr("x", labelX)
-        .attr("y", labelY)
-        .attr("text-anchor", "middle")
-        .attr("font-size", "12px")
-        .attr("fill", "#666")
-        .attr("class", "axis-label")
-        .text(genre);
-    });
+    const radarUsersPath = radarGroup
+      .selectAll("g")
+      .data(userData)
+      .join("g")
+      .attr("data-user", ({ user_id }) => user_id);
 
-    // Fonction pour dessiner le radar pour chaque utilisateur
-    function drawRadar({ user_id, username, data: userData }) {
-      const radarLine = d3
-        .lineRadial()
-        .radius((d) => scale(d.proportion))
-        .angle((d, i) => {
-          const angleIndex = allAxes.indexOf(d.name);
-          return angleSlice * angleIndex;
-        });
+    const radar = d3
+      .lineRadial<{
+        name: string;
+        proportion: number;
+      }>()
+      .radius(({ proportion }) => scale(proportion))
+      .angle(({ name }) => allAxes.indexOf(name) * angleSlice);
 
-      const radarArea = radarLine(userData);
-
-      const radarPath = radarGroup
-        .append("path")
-        .attr("d", radarArea + "Z")
-        .attr("transform", `translate(${centerX}, ${centerY})`)
-        .attr("class", "radar-area")
-        .style("fill", colors[user_id])
-        .style("stroke", colors[user_id])
-        .style("opacity", 0.5);
-
-      radarPath
-        .on("mouseover", function () {
-          d3.select(this).style("opacity", 0.8).style("stroke-width", "2px");
-          tooltip.style("visibility", "visible").html(
-            `<div style="display: flex; align-items: center;">
+    radarUsersPath
+      .selectAll("path")
+      .data(({ genres, user_id, username }) => [
+        {
+          user_id,
+          username,
+          path: radar(genres) + "Z",
+        },
+      ])
+      .join("path")
+      .attr("d", ({ path }) => path)
+      .attr("transform", `translate(${centerX}, ${centerY})`)
+      .attr("class", "radar-area")
+      .style("fill", ({ user_id }) => colors[user_id])
+      .style("stroke", ({ user_id }) => colors[user_id])
+      .style("opacity", 0.5)
+      .on("mouseover", function (_, { username, user_id }) {
+        d3.select(this).style("opacity", 0.8).style("stroke-width", "2px");
+        tooltip.style("visibility", "visible").html(
+          `<div style="display: flex; align-items: center;">
               <div style="width: 10px; height: 10px; background-color: ${colors[user_id]}; margin-right: 5px; border: 1px solid #000;"></div>
               <strong>${username} </strong><br>
             </div>`
-          );
+        );
+      })
+      .on("mousemove", (event) => {
+        tooltip
+          .style("top", `${event.pageY - 20}px`)
+          .style("left", `${event.pageX + 10}px`);
+      })
+      .on("mouseout", function () {
+        d3.select(this).style("opacity", 0.5).style("stroke-width", "1px");
+        tooltip.style("visibility", "hidden");
+      });
+
+    const radarUsersCircle = pointsGroup
+      .selectAll("g")
+      .data(userData)
+      .join("g")
+      .attr("data-user", ({ user_id }) => user_id);
+
+    radarUsersCircle
+      .selectAll("circle")
+      .data(({ genres, ...data }) =>
+        genres.map((genreData) => {
+          const angle =
+            angleSlice * allAxes.indexOf(genreData.name) + (3 * Math.PI) / 2;
+          const x = centerX + Math.cos(angle) * scale(genreData.proportion);
+          const y = centerY + Math.sin(angle) * scale(genreData.proportion);
+          return { ...data, ...genreData, x, y };
         })
-        .on("mousemove", (event) => {
-          tooltip
-            .style("top", `${event.pageY - 20}px`)
-            .style("left", `${event.pageX + 10}px`);
-        })
-        .on("mouseout", function () {
-          d3.select(this).style("opacity", 0.5).style("stroke-width", "1px");
-          tooltip.style("visibility", "hidden");
-        });
+      )
+      .join("circle")
+      .attr("cx", ({ x }) => x)
+      .attr("cy", ({ y }) => y)
+      .attr("r", pointSize(d3.zoomTransform(zoomGroup.node()!).k))
+      .style("fill", ({ user_id }) => colors[user_id])
+      .style("cursor", "pointer")
+      .on(
+        "mouseover",
+        function (_, { user_id, formatedTime, name, username, proportion }) {
+          d3.select('[data-user="' + user_id + '"]').raise(); // Déplace l'utilisateur au-dessus des autres
 
-      // S'assurer qu'il y ait un point pour chaque axe, même avec une proportion de 0
-      allAxes.forEach((axis, i) => {
-        const dataPoint = userData.find((d) => d.name === axis);
-        const value = dataPoint ? dataPoint.proportion : 0;
-        const temps = dataPoint ? dataPoint.temps : 0;
-
-        const angle = angleSlice * i + (3 * Math.PI) / 2;
-        const x = centerX + Math.cos(angle) * scale(value);
-        const y = centerY + Math.sin(angle) * scale(value);
-
-        pointsGroup
-          .append("circle")
-          .attr("cx", x)
-          .attr("cy", y)
-
-          .style("fill", colors[user_id])
-          .style("cursor", "pointer")
-          .on("mouseover", function () {
-            radarPath.raise(); // Déplace la zone radar au-dessus des autres
-
-            tooltip.style("visibility", "visible").html(
-              `<div style="display: flex; align-items: center;">
-                  <div style="width: 10px; height: 10px; background-color: ${colors[user_id]}; margin-right: 5px; border: 1px solid #000;"></div>
+          tooltip.style("visibility", "visible").html(
+            `<div style="display: flex; align-items: center;">
+                  <div style="width: 10px; height: 10px; background-color: ${
+                    colors[user_id]
+                  }; margin-right: 5px; border: 1px solid #000;"></div>
                   <strong>${username}</strong><br>
                 </div>
-                <strong>Genre:</strong> ${axis}<br>
-                <strong>Proportion:</strong> ${value}%<br>
-                <strong>Temps:</strong> ${temps}`
-            );
-
-            // Augmenter dynamiquement le rayon
-            const currentRadius = parseFloat(d3.select(this).attr("r")); // Récupérer le rayon actuel
-            d3.select(this).attr("r", currentRadius + 1); // Ajouter 1 au rayon actuel
-          })
-          .on("mousemove", (event) => {
-            tooltip
-              .style("top", `${event.pageY - 20}px`)
-              .style("left", `${event.pageX + 10}px`);
-          })
-          .on("mouseout", function () {
-            tooltip.style("visibility", "hidden");
-            d3.select(this).attr(
-              "r",
-              pointSize(d3.zoomTransform(zoomGroup.node()).k)
-            );
-          })
-          .on("click", () => {
-            // Redirection vers la page HTML correspondante
-            //TODO:
-            // user
-            // axis
-            // period
-          });
-      });
-    }
-
-    // Afficher les radars pour les utilisateurs sélectionnés
-    selectedUsers.forEach((user) => {
-      const userData = data.users.find((u) => u.user_id === user);
-      if (!userData) return;
-      const userFiltered = allAxes.map((axis) => {
-        // Recherche dans les différents "top_genres"
-        const match = userData.top_genres.find((genreData) => {
-          // On cherche la période correspondant au label sélectionné
-          if (genreData.label === period) {
-            // Si la période correspond, on parcourt le tableau "ranking" de cette période pour chercher le genre
-            return genreData.ranking.some(
-              (genre) => genre.Tags.toLowerCase() === axis.toLowerCase()
-            );
-          }
-          return false; // Si la période ne correspond pas, on ne fait pas de match
-        });
-
-        if (match) {
-          // Calcul du total des temps d'écoute de tous les genres dans cette période
-          const totalListeningTime = match.ranking.reduce(
-            (total, genre) => total + genre["Listening Time"],
-            0
+                <strong>Genre:</strong> ${name}<br>
+                <strong>Proportion:</strong> ${proportion.toFixed(1)}%<br>
+                <strong>Temps:</strong> ${formatedTime}`
           );
 
-          // Recherche du genre spécifique dans le ranking de cette période
-          const genreMatch = match.ranking.find(
-            (genre) => genre.Tags.toLowerCase() === axis.toLowerCase()
-          );
-
-          // Calcul de la proportion de ce genre par rapport au total
-          return {
-            name: axis,
-            // Calcul de la proportion, arrondi à 1 ou 0 décimale
-            proportion: genreMatch
-              ? (
-                  (genreMatch["Listening Time"] / totalListeningTime) *
-                  100
-                ).toFixed(
-                  genreMatch["Listening Time"] % totalListeningTime === 0
-                    ? 0
-                    : 1
-                )
-              : 0,
-
-            // Temps d'écoute formaté en heures et minutes
-            temps: formatListenTime(genreMatch["Listening Time"]),
-          };
-        } else {
-          //valeurs par défaut
-          return {
-            name: axis,
-            proportion: 0,
-            temps: "0min",
-          };
+          // Augmenter dynamiquement le rayon
+          const currentRadius = parseFloat(d3.select(this).attr("r")); // Récupérer le rayon actuel
+          d3.select(this).attr("r", currentRadius + 1); // Ajouter 1 au rayon actuel
         }
+      )
+      .on("mousemove", (event) => {
+        tooltip
+          .style("top", `${event.pageY - 20}px`)
+          .style("left", `${event.pageX + 10}px`);
+      })
+      .on("mouseout", function () {
+        tooltip.style("visibility", "hidden");
+        d3.select(this).attr(
+          "r",
+          pointSize(d3.zoomTransform(zoomGroup.node()!).k)
+        );
       });
-      drawRadar({
-        user_id: userData.user_id,
-        username: userData.username,
-        data: userFiltered,
-      });
-    });
 
     const zoom = d3
-      .zoom()
+      .zoom<SVGSVGElement, unknown>()
       .scaleExtent([1, 10])
       .translateExtent([
         [width / 4, height / 4],
         [(width * 3) / 4, (height * 3) / 4],
       ])
-      .on("zoom", function (event) {
-        svg.selectAll(".zoom-group").attr("transform", event.transform);
+      .on("zoom", (event) => {
         svg
+          .selectAll(".zoom-group")
+          .attr("transform", event.transform)
           .selectAll(".points-group circle")
           .attr("r", pointSize(event.transform.k));
       });
     svg.call(zoom);
     const newTransform = d3.zoomIdentity.translate(-width, -height).scale(3);
     svg.transition().duration(750).call(zoom.transform, newTransform);
-  }, [selectedUsers, period, topN, container, allAxes, width, height]);
+  }, [
+    selectedUsers,
+    period,
+    topN,
+    container,
+    allAxes,
+    width,
+    height,
+    userData,
+  ]);
 
   return (
     <Grid2
